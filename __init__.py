e4a0e9998e7b2a87db87cc073705b6d6c3cd8f0b
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2026-04-08 21:07:23 +1000

    __init__.py import from actual source
    This seems to make it easier for variantopia to import the GFF3Tarium class

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from modules.domains import Domains
 from modules.coordinates import OverlapResolver
-from modules.gff3 import GFF3Feature, GFF3Tarium
+from modules.gff3tarium import GFF3Feature, GFF3Tarium
 from modules.fasta import TranslationTable, Sequence, Records, FASTATarium
